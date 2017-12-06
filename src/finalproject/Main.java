aae0352b41936549fcf43a2418f97f07f19cbaa2
MightyMitch22 <MitchBlier18@my.uri.edu>
2017-12-05 20:27:25 -0500

    Added code from Professor Hervé's Bouncing Ball code.

--- src/finalproject/Main.java
+++ src/finalproject/Main.java
@@ -6,4 +6,31 @@ package finalproject;
  */
 public class Main {
 
+/**
+ * settings will create the window in which the
+ * app will take place
+ * *
+ */
+  public void settings(){
+
+    //Initial Scene configuration
+    size(WINDOW_WIDTH, WINDOW_HEIGHT, P3D);
+
+  }
+  /**
+   * drawSurface will create the ground where the monster will stand
+   *
+   */
+  public void drawSurface(){
+    beginShape(QUADS);
+    texture(backgroundImage_);
+
+    vertex(XMIN, YMAX, 0, 0, 0);
+    vertex(XMIN, YMIN, 0, 0, 1);
+    vertex(XMAX, YMIN, 0, 1, 0);
+    vertex(XMAX, YMAX, 0, 1, 1);
+
+    endShape(CLOSE);
+  }
+
 }
--- src/finalproject/Monster.java
+++ src/finalproject/Monster.java
@@ -10,9 +10,14 @@ package finalproject;
  */
 public class Monster extends GraphicObject implements ApplicationConstants, AnimatedObject {
 
+  private float bx_ = -20, by_ = 0, bz_ = 30;
+  private float Vx_ = 12, Vy_ = 0, Vz_ = 0;
+  private float rad_ = 5;
+  private float refl_ = 0.8f;
+  private static final float ZERO_SPEED = 0.01f;
+
     /**
      * The Monster will need to keep track of its own location which the main will update.
-     *
      */
     public Monster(){
 
@@ -25,6 +30,10 @@ public class Monster extends GraphicObject implements ApplicationConstants, Anim
     public void draw(){
         app_.pushMatrix();
 
+        app.translaate(bx_, by_, bz_);
+        app.noStroke();
+        app.sphere(rad_);
+
         app_.popMatrix();
     }
 
@@ -35,8 +44,29 @@ public class Monster extends GraphicObject implements ApplicationConstants, Anim
      */
     public void update(float dt){
 
+      if (bz_ <= rad_) {
+       Vz_ = refl_ * PApplet.abs(Vz_);
+       Vx_ *= refl_;
+       Vy_ *= refl_;
 
-    }
+       if (PApplet.abs(Vx_) < ZERO_SPEED)
+           Vx_ = 0.f;
+       if (PApplet.abs(Vy_) < ZERO_SPEED)
+           Vy_ = 0.f;
+       if (PApplet.abs(Vz_) < ZERO_SPEED)
+           Vz_ = 0.f;
+
+         }
+
+      float halfdt2 = 0.5f * dt*dt;
+
+      bx_ += Vx_ * dt;
+      by_ += Vy_ * dt;
+      bz_ += Vz_ * dt - G*halfdt2;
+
+      Vz_ -= G * dt;
+
+      }
 
     /**
      * We need to have a method that detects when the Monster is touched
